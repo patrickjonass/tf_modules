ca9a76ef331772621ef5a693cb67c7dd6d9c2ba5
pjatibayan <pat.tibayan@gmail.com>
2023-11-04 07:09:52 +0800

    fix: add initial variables

---
+++ ec2/variables.tf
@@ -0,0 +1,59 @@
+variable "ami" {
+    description = ""
+    default = []
+}
+
+variable "associate_public_ip_address" {
+    description = ""
+    default = []
+}
+
+variable "availability_zone" {
+    description = {}
+    default = []
+}
+
+variable "ebs_block_device" {
+    description = {}
+    default = []
+}
+
+variable "iam_instance_profile" {
+    description = {}
+    default = []
+}
+
+variable "instance_type" {
+    description = {}
+    default = []
+}
+
+variable "key_name" {
+    description = {}
+    default = []
+}
+
+variable "root_block_device" {
+    description = {}
+    default = []
+}
+
+variable "subnet_id" {
+    description = {}
+    default = []
+}
+
+variable "tags" {
+    description = {}
+    default = []
+}
+
+variable "user_data" {
+    description = {}
+    default = []
+}
+
+variable "vpc_security_group_ids" {
+    description = {}
+    default = []
+} 
